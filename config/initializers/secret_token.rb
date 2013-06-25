a5c572cf0895894959edf147e701c67a2a0294a9
Super-User <root@urnews1.my.micloud.tw>
2013-06-25 12:15:17 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caitblog::Application.config.secret_token = '7d2338fb2897faf474db035224d11b738eb050e2862a3170ec0c378595e46eeeea7663c78e937a77708af2164d790e20c4c34575874678d14782ebf250c99c69'
